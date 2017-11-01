614f2967aef4401ae0feddca7af5554dd9c53cb5
Ganesh <Mahendra@192.168.0.5>
2017-11-01 18:45:22 +0530

    Create Table in oracle through JDBC

---
+++ src/CreateTable.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CreateTable {
+
+	public static void main(String[] args) throws SQLException, IOException {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle");
+			
+			Statement st=con.createStatement();
+			BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter table Name");
+			String tname=br.readLine();
+			String sql="create table "+ tname + "(eno number,ename varchar2(15),esal number)";
+//			String sql = "CREATE TABLE" + tname  +
+//	                   "(id INTEGER not NULL, " +
+//	                   " first VARCHAR(255), " + 
+//	                   " last VARCHAR(255), " + 
+//	                   " age INTEGER, " + 
+//	                   " PRIMARY KEY ( id ))"; 
+			st.executeUpdate(sql);
+			System.out.println("table created successfully");
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
